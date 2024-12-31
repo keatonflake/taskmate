b94c8d3a648e65b0c1793d9a4838ed3b6e2ff7cf
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2024-12-30 21:11:30 -0700

    added basic task state management for tasks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
-import "App.css";
+import "./App.css";
+import { useState } from "react";
 
-export default App = () => {
-  return <h1>Hello!</h1>;
-};
+function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, name: "Learn React Like a Pro", completed: false },
+    { id: 2, name: "Learn React Like a champ", completed: false },
+    { id: 3, name: "Learn React Like a wizard", completed: false },
+    { id: 4, name: "Learn React Like a stud", completed: true },
+  ]);
+
+  const [show, setShow] = useState(true);
+
+  function handleDelete(id) {
+    setTasks(tasks.filter((task) => id !== task.id));
+  }
+
+  return (
+    <div className="App">
+      <h1>Task List</h1>
+      <ul>
+        <button className="trigger" onClick={() => setShow(!show)}>
+          Toggle
+        </button>
+        {show &&
+          tasks.map((task) => (
+            <li
+              key={task.id}
+              className={task.completed ? "completed" : "incomplete"}
+            >
+              <span>
+                {task.id} - {task.name}
+              </span>
+              <button onClick={() => handleDelete(task.id)} className="delete">
+                Delete
+              </button>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
